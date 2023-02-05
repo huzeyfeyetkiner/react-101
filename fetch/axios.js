602a4ad0969d829771fb2221b8aede1542cd53ad
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-02-05 17:45:20 +0300

    axios code added file name changed

--- fetch/axios.js
+++ fetch/axios.js
@@ -1,3 +1,22 @@
 
 
+// fetch yapısından farklı olarak gelen datayı json ile dönüştürmemiz gerekmiyor.
+
+
 import axios from "axios";
+
+(async ()=>{
+
+    const {data : users} = await axios("https://jsonplaceholder.typicode.com/users")
+
+    const {data : post1} = await axios("https://jsonplaceholder.typicode.com/posts/1")
+
+    const {data : post2} = await axios("https://jsonplaceholder.typicode.com/posts/2")
+
+
+
+    console.log("Users yüklendi ",users)
+    console.log("Post1 yüklendi ",post1)
+    console.log("Post2 yüklendi ",post2)
+
+})();
