edc5df8722af6b84fb6c86618f23c2605fc7b3c0
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-02-08 17:27:41 +0300

    props subject

--- first-react/hello-react/src/App.js
+++ first-react/hello-react/src/App.js
@@ -4,9 +4,30 @@ import Header from "./components/header.js";
 import User from "./components/user.js"
 
 
-const name = "Huzeyfe"
-const surName = "Yetkiner"
-const isLoggedIn = false
+// const name = "Huzeyfe"
+// const surName = "Yetkiner"
+// const isLoggedIn = false
+
+
+const friends = [
+  {
+    id:1,
+    name:"Burak"
+  },
+  {
+    id:2,
+    name:"Muhammet"
+  },
+  {
+    id:3,
+    name:"Samet"
+  },
+]
+
+const address = {
+  title: "Eyüp/İstanbul",
+  zip : 34050
+}
 
 function App() {
   return (
@@ -23,13 +44,20 @@ function App() {
       <input type="text" id="userName" className="qwe"/>
 
       {/* değişken yazma */}
-      <h1>{name} {surName}</h1>
-      <h1>{`Benim adım ${name}, soyadım ${surName}`}</h1>
+      {/* <h1>{name} {surName}</h1> */}
+      {/* <h1>{`Benim adım ${name}, soyadım ${surName}`}</h1> */}
 
       {/* koşullu değişken render işlemi */}
-      <h1>{ isLoggedIn ? `Benim adım ${name}, soyadım ${surName}`:"giriş yapmadınız"}</h1>
+      {/* <h1>{ isLoggedIn ? `Benim adım ${name}, soyadım ${surName}`:"giriş yapmadınız"}</h1> */}
 
-      <User name="Furkan" surName ="Yetkiner" age={21} isLoggedIn={true}></User>
+      <User 
+      name="Furkan" 
+      surName ="Yetkiner" 
+      age={"21"} 
+      // isLoggedIn={true} 
+      friends={friends} 
+      address={address}>        
+      </User>
     </>
   );
 }
--- first-react/hello-react/src/components/user.js
+++ first-react/hello-react/src/components/user.js
@@ -1,4 +1,4 @@
-
+import propTypes from "prop-types";
 
 //props parametresi ile oluşturduğumuz componente verdiğimiz propertieslere erişebiliyoruz
 // function User(props){
@@ -9,11 +9,46 @@
 // }   
 
 // yukarıdaki kullanımdan farklı olarak props parametresi bize obje döndürdüğünden obje içerisindeki elemanlara aşağıdaki gibi de erişebiliriz.
-function User({name, surName, age, isLoggedIn}){
+function User({name, surName, age, isLoggedIn, friends, address}){
+
+    if(!isLoggedIn){
+        return <h1>Giriş Yapmadınız</h1>
+    }
+
     return(
-        <h1>{isLoggedIn ? `${name} ${surName} ${age}` : `Giriş yapmadınız`}</h1>
+        <>
+            <h1>{isLoggedIn ? `${name} ${surName} ${age}` : `Giriş yapmadınız`}</h1>
+            
+            <p>{address.title} {address.zip}</p>
+            
+
+            {/* liste obje gibi döngüye sokmamız gereken propların unique key propları olmak zorunda aksi halde console ekranı üzerinde hatayla karşlılaşıyoruz  */}
+            {
+                friends.map(friend => <div key={friend.id}>{friend.name}</div>)
+            }          
+            
+            
+        </>
+        
     )
    
 }  
+
+
+// prop type özelliği sayesinde componentime gelen propların hangi veri türüne sahip olduğunu belirtiriz
+User.propTypes = {
+    name: propTypes.string.isRequired, //isRequired ile beraber name propunun gönderilmesinin zorunlu olduğu belirtilir
+    surName: propTypes.string,
+    age: propTypes.oneOfType([propTypes.number, propTypes.string]), // oneOfType anahtar sözcüğüyle propla gönderilen verinin farklı türleri kabul etmesi sağlanır
+    friends: propTypes.array,
+    address: propTypes.shape({ //obje içerisinde birden fazla değer tutulduğu için her bir değerin proptype'nı belirtmek için kullanılır
+        title: propTypes.string,
+        zip: propTypes.number,
+    })
+}
+// herhangi bir veri yollanmadığı taktirde defaultProps ile beraber proplara varsayılan değerler atayabiliyoruz.
+User.defaultProps = {
+    isLoggedIn: false
+}
 
 export default User
