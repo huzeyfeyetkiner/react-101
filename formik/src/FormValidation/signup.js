85cbf42ab7326a79af4c781991caadfd227c70dd
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-02-20 19:03:06 +0300

    error messages with yup

--- formik/src/FormValidation/signup.js
+++ formik/src/FormValidation/signup.js
@@ -2,7 +2,7 @@ import {useFormik} from "formik"
 import validationSchema from "./validations"
 
 function SignUp() {
-    const {handleChange, handleSubmit, values}= useFormik({
+    const {handleChange, handleSubmit, handleBlur, touched, values, errors}= useFormik({
         initialValues: {
         email: '',
         password: '',
@@ -15,31 +15,19 @@ function SignUp() {
 });    
   return (
     <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>            
             
-            
-            <label>Email</label>
-            <input type="email" name='email' value={values.email} onChange={handleChange}/>
+            <div>Email</div>
+            <input type="email" name='email' value={values.email} onBlur={handleBlur} onChange={handleChange}/>
+            { errors.email && touched.email && ( <div className="error">{errors.email}</div> ) }
 
-            <label>Password</label>
-            <input type="text" name='password' value={values.password} onChange={handleChange} />
+            <div>Password</div>
+            <input type="text" name='password' value={values.password} onBlur={handleBlur} onChange={handleChange} />
+            { errors.password && touched.password && ( <div className="error">{errors.password}</div> ) }
 
-            <label htmlFor="lastName">Confirm Password</label>
-            <input type="text" name='confirmPassword' value={values.confirmPassword} onChange={handleChange} />
-
-              
-
-              
-
-              <br /><br />  
-             
-
-
-              <br /><br />
-
-             
-
-              <br /><br />
+            <div htmlFor="lastName">Confirm Password</div>
+            <input type="text" name='confirmPassword' value={values.confirmPassword} onBlur={handleBlur} onChange={handleChange} />
+            { errors.confirmPassword && touched.confirmPassword && ( <div className="error">{errors.confirmPassword}</div> ) }
 
               <button type="submit">Submit</button>
               <br />
--- formik/src/FormValidation/validations.js
+++ formik/src/FormValidation/validations.js
@@ -1,9 +1,9 @@
 import { object, string, number, date, InferType, ref } from 'yup';
 
 const validations = object({    
-    email: string().email().required(),
-    password: string().min(5).required(),
-    confirmPassword: string().oneOf([ref("password")]).required()
+    email: string().email("E-mail must be valid").required("Required"),
+    password: string().min(5,"Minimum 5").required(),
+    confirmPassword: string().oneOf([ref("password")],"passwords are not matching").required()
     
   });
 
