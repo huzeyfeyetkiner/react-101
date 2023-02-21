e6fc080c3bb85e26a7a94820c747f74e6e274c30
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-02-21 17:29:46 +0300

    useMemo reason of usage

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -10,15 +10,16 @@ function App() {
 
   // useMemo ile beraber yukarıdaki gibi component her re-render olduğunda obje adresinin değişmesini engelleyebiliriz.
   // dependencies array içerisine bir state girdiğimiz taktirde o state üzerinde herhangi bir değişiklik olduğunda obje adresinin değişmesini sağlayarak ilgili component/componentların re-render edilmesini de sağlayabiliriz.
-  const data = useMemo(() => {
-    return {name: "Huzeyfe"}
-  }, [])
-
-  // const data = useMemo(() => {
-  //   return {name: "Huzeyfe"}
-  // }, [num]) yorum satırı içerisindeki gibi bir kullanım söz konusu olduğunda num içerisindeki veri her değiştiğinde objemizin referans adreside değişikliğe uğrayacağından ilgili componentler re-render edilecek
+  
+  // useMemo genellikle arraylar ve objeler için kullanılıyor
 
   const [num, setNum] = useState(0)
+  const [text, setText] = useState("")
+
+  const data = useMemo(() => {
+    getObject();
+  }, [num])
+  // sadece num state'i değiştiği zaman objeye bağlı componentlar re-render edilecek
 
   return (
     <div className="App">
@@ -26,8 +27,20 @@ function App() {
       <hr />
       <h1>{num}</h1>
       <button onClick={() => setNum(num + 1)}>Increase</button>
+      <br />
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
     </div>
   );
+}
+
+// useMemonun kullanım mantığını anlamak için tanımlanmış fonksiyon
+// örneğin bizim objemizi elde etmek için uzun süreli işlemlerden geçmemiz gerekiyor bu nedenle bu objeyi component içerisindeki her state değişikliğinde yeni bir adrese göndermek ve re-render işlemine maruz kalmasını istemiyorum. Bu nedenle bu objenin değişmesi gereken durumları useMemo ile belirleyebiliyorum
+function getObject() {
+  console.log("Loading...")
+  for(let i=0;i<10000000;i++){}
+  console.log("Loading Completed")
+
+  return {name:"Huzeyfe"}
 }
 
 export default App;
