da2e94ee66445bbc43b9686f8fa4b4f1566ce8d8
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-02-22 23:47:03 +0300

    structure of route

--- overview/src/App.js
+++ overview/src/App.js
@@ -4,13 +4,18 @@ import MainLayout from "./layouts/mainLayout";
 import Signup from "./Pages/signup";
 import Home from "./Pages/home";
 import "./App.css"
+import Users from "./Pages/users";
+import Products from "./Pages/products";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={ <MainLayout /> } > 
-          <Route path="/home" element={ <Home /> } /> 
+          <Route path="/home" element={ <Home /> } > 
+            <Route path="users" element={ <Users /> } /> 
+            <Route path="products" element={ <Products /> } /> 
+          </Route> 
           <Route path="/signup" element={ <Signup /> } /> 
         </Route>
         
--- overview/src/Pages/home.js
+++ overview/src/Pages/home.js
@@ -1,6 +1,16 @@
+import { NavLink, Outlet } from "react-router-dom"
+
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home</h1>
+
+        <NavLink to={"users"}>Users</NavLink>
+        <NavLink to={"products"}>Products</NavLink>
+
+        <Outlet />
+
+    </div>
   )
 }
 export default Home
