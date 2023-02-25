c47a1068578415d0679a1224d7882dc735878542
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-02-25 18:23:29 +0300

    api connection

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Dropdown from './components/dropdown';
 import Content from './components/content';
@@ -7,14 +6,6 @@ import { WeatherProvider } from './context/WeatherContext';
 
 function App() {
 
-
-  // const [weather, setWeather] = useState({});
-
-  // useEffect(() => {
-  //   axios("https://api.openweathermap.org/data/3.0/onecall?lat=37.0000&lon=35.3213&exclude=hourly,daily&appid=52cb0c406e77c3b4de742e25be54c7f0")
-  //   .then(resp => setWeather(resp))
-  //   console.log(weather.data.current.weather[0].main);
-  // }, [])
   return (
     <WeatherProvider>
       <Dropdown />
