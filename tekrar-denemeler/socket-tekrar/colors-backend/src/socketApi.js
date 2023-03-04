45a3b00ac99ba8e89c85d7c1b0a58614361cb2e9
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-03-04 17:12:59 +0300

    realtime applications overviewed with socket.io

---
+++ tekrar-denemeler/socket-tekrar/colors-backend/src/socketApi.js
@@ -0,0 +1,27 @@
+import io from "socket.io-client"
+
+let socket
+
+export const init = () => {
+    
+    console.log("Bağlanılıyor");
+    socket = io("http://localhost:3001",{
+        transports: ["websocket"]
+    })
+
+    socket.on("connect", () => {
+        console.log("Bağlantı gerçekleşti");
+    })
+
+}
+
+export const send = (color) => {
+    socket.emit("newColor",color)
+}
+
+export const subscribe = (cb) => {
+    socket.on("receive", (color) => {
+        console.log(color);
+        cb(color)
+    })
+}
