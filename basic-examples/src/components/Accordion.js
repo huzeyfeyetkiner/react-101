130dea020232e45eb6d8883a80d486163b0b517b
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-03-14 17:16:34 +0300

    redo accordion

--- basic-examples/src/components/Accordion.js
+++ basic-examples/src/components/Accordion.js
@@ -1,30 +1,25 @@
 import { useState } from "react"
 
-function Accordion({title, answer}) {    
+function Accordion({title, answer}) {
 
     const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className="accordion-container">
-
         <div className="accordion-item">
-                    <div className="accordion-header" onClick={() => setIsOpen(!isOpen)}>
-                        <span className="question">{title}</span>
-                        <span className="btn-container" >{isOpen ? "-" : "+"}</span>
-                    </div>
-                {
-                    isOpen &&
-                    <div className="accordion-content">{answer}</div>
-                }
+            <div className="accordion-header" onClick={() => setIsOpen(!isOpen)}>
+                <span>{title}</span>
+                <span className="btn-container">{isOpen ? "-" : "+"}</span>
+            </div>
+            {
+                isOpen &&
+                <div className="accordion-content">
+                    {answer}
+                </div>
+            }
+            
         </div>
 
-        
-        
-       
-
-        
-
-        
     </div>
   )
 }
--- basic-examples/src/pages/AccordionPage.js
+++ basic-examples/src/pages/AccordionPage.js
@@ -1,34 +1,40 @@
-import Accordion from "../components/Accordion"
+import  Accordion  from "../components/Accordion.js"
+
 
 
 function AccordionPage() {
 
     const questions = [
         {
-            title: "Yeni Koronavirüs (SARS-CoV-2) nedir?",
-            answer: "Yeni Koronavirüs (SARS-CoV-2) ilk olarak Çin’in Vuhan şehrinde tespit edilen ve  solunum yolu enfeksiyonu yapan bir virüstür; COVID-19’a sebep olur. Bu hastalık yüksek ateş, öksürük, nefes darlığı, baş ağrısı, boğaz ağrısı, burun akıntısı, kas ve eklem ağrısı, halsizlik, koku ve tat alma duyusu kaybı, ishal gibi belirtilerle kendini gösterir."
+            title: "ÖSYM Aday İşlemleri Sistemi (AİS) şifresi olmayan adaylar, bireysel olarak elektronik ortamda 2023-YKSye başvuru yapabilirler mi?",
+            answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eligendi dicta dolorem, similique ducimus modi odit unde ipsam numquam quae mollitia, architecto, perspiciatis sed dolorum blanditiis quod doloremque doloribus iure sequi obcaecati soluta officia nostrum harum asperiores! Tenetur, dolorum minus! Officia modi tenetur perferendis vero quasi eaque exercitationem sint ipsam?"
         },
         {
-            title: "COVID-19 nasıl bulaşır?",
-            answer: "COVID-19, hasta kişilerin öksürme, hapşırma veya konuşmayla ortaya saçtığı damlacıkların ortamdaki diğer bireyler tarafından solunması, damlacıkların yapıştığı yüzeylere dokunduktan sonra ellerin ağız, burun veya göze götürülmesiyle bulaşır."
+            title: "ÖSYM Başvuru Merkezlerine gitmeden 2023-YKS’ye başvuru yapmak mümkün mü?",
+            answer: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, officiis laboriosam eius, vel fugit vitae dolor amet doloribus rem impedit quasi labore placeat praesentium culpa ratione architecto magni saepe odit?"
         },
         {
-            title: "COVID-19'un tanısı nasıl konulur?",
-            answer: "Kesin tanı moleküler testlerle konulmaktadır. Bu testler kişiden alınan boğaz ve/veya burun sürüntüsünde virüsün genetik materyalinin aranması prensibine dayanır. Ülkemizde tanı PCR testi ile konulmaktadır."
-        }
+            title: "ÖSYM Aday İşlemleri Sisteminde (AİS) güncel fotoğrafı olmayan adaylar 2023-YKS’ye başvuru yapabilirler mi?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem reiciendis facilis repellendus eum neque rerum fugit, natus ad, sequi iure, omnis pariatur numquam quia quos minima alias. Eligendi quia perspiciatis deserunt repudiandae, fugiat amet fuga ducimus explicabo ea numquam, aliquid quo, facilis pariatur dicta porro veniam consequatur magnam? Corporis beatae sed, consequuntur veniam adipisci quia. Neque tempora ex officia beatae."
+        },
+        {
+            title: "ÖSYM Mobil uygulamasından 2023-YKS başvurusu yapılabilir mi?",
+            answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iusto ipsum dignissimos ipsam itaque. Neque dolores cupiditate, nostrum natus blanditiis at ullam mollitia vero provident id quis autem eaque laborum fuga alias quo maxime necessitatibus sed sapiente nulla pariatur suscipit? Ad odio placeat mollitia sunt!"
+        },
     ]
-
 
   return (
     <div>
+
         {
-            questions.map((question, index) => {
-                return(
-                    <Accordion key={index} title={question.title} answer={question.answer} />
-                )
-            })
+        questions.map((question, index) => {
+            return(
+                <Accordion key={index} title={question.title} answer={question.answer}/>
+            )
+        })
         }
-       
+        
+
     </div>
   )
 }
