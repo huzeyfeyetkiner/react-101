a123cab62a52868a7f2939429025ee66b021a98d
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-03-20 18:01:37 +0300

    copy to clipboard func added

--- colors-generator/src/components/Color.js
+++ colors-generator/src/components/Color.js
@@ -1,15 +1,33 @@
+import {useState} from "react"
+
 function Color({color, index}) {
 
+    const [alert, setAlert] = useState(false)
     let textColor = index < 10 ? "black" : "white"
 
+    const copyClipboard = ()=>{
+        navigator.clipboard.writeText(`#${color.hex}`)
+        setAlert(true)
+        setInterval(() => {
+            setAlert(false)
+        },3000)
+    }
+
   return (
-    <div className="color-container" style={{backgroundColor: `#${color.hex}`}}>
+    <div 
+    className="color-container" 
+    style={{backgroundColor: `#${color.hex}`}}
+    onClick={copyClipboard}
+    >
 
         <p style={{color:textColor
         }}>%{color.weight}</p>
         <p style={{color:textColor
         }}>#{color.hex}</p>
 
+        {
+            alert && <p style={{color:textColor}} className="alert">Coppied to Clipboard</p>
+        }
     </div>
   )
 }
