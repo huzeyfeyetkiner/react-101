7c4da4530c1ab20c00b25cb237cb0891f9eb15f8
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-03-22 18:07:42 +0300

    del and complete function, minor css

--- grocery-bud/src/components/List.js
+++ grocery-bud/src/components/List.js
@@ -2,25 +2,68 @@ import { useList } from "../context/ListContext"
 
 function List() {
 
-    const { list } = useList()
-
-  return (
+    const { list, setList } = useList()
+  return (  
     <ul className="list">
         {
+
             list.map((item, index) => {
-                return(
-                    <li key={index}>
-                        <div className="list-item">
-                            <input type="checkbox" name="" id="" />
-                            <span>{item.title}</span>
-                            <div className="buttons">
-                                <button className="btn-edit">Edit</button>
-                                <button className="btn-del">Delete</button>
+                if(item.completed === false) {
+                    return(
+                        <li key={index}>
+                            <div className="list-item">
+
+                                <input type="checkbox" name="" id="" onChange={() => {
+                                    const newArray = list;
+                                    newArray[index].completed = true 
+                                    setList([...newArray])
+                                }} checked={false}/>
+
+                                <span>{item.title}</span>
+                                <div className="buttons">
+                                    <button className="btn-edit">Edit</button>
+
+                                    <button 
+                                    className="btn-del"
+                                    onClick={() => {
+                                        const newArray = list;
+                                        newArray.splice(index,1)
+                                        setList([...newArray])
+                                    }}>Delete</button>
+                                </div>
+                                
                             </div>
-                            
-                        </div>
-                    </li>
-                )
+                        </li>
+                    )
+                }else{
+                    return(
+                        <li key={index}>
+                            <div className="list-item completed">
+
+                                <input type="checkbox" name="" id="" onChange={() => {
+                                    const newArray = list;
+                                    newArray[index].completed = false 
+                                    setList([...newArray])
+                                }} checked={true}/>
+
+                                <span>{item.title}</span>
+
+                                <div className="buttons">
+                                    <button className="btn-edit">Edit</button>
+                                    <button 
+                                    className="btn-del"
+                                    onClick={() => {
+                                        const newArray = list;
+                                        newArray.splice(index,1)
+                                        setList([...newArray])
+                                    }}>Delete</button>
+                                </div>
+                                
+                            </div>
+                        </li>
+                    )
+                }
+                
                 
             })
         }
