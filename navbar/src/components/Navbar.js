e35c5ca1676c299a2a87921515f345cecd67cab7
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-03-25 20:14:35 +0300

    navbar with useRef

--- navbar/src/components/Navbar.js
+++ navbar/src/components/Navbar.js
@@ -1,24 +1,41 @@
 import { links, social } from "../data";
 import { FaBars } from "react-icons/fa";
 import logo from "../logo.svg";
+import { useEffect, useRef, useState } from "react";
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (toggle) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [toggle]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
-              const { id, url, text } = link;
               return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
                 </li>
               );
             })}
