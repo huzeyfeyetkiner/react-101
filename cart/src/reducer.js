0d978cb2bf075fb973b7a6f799003dc9b77c223b
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-03-31 20:49:07 +0300

    clear, remove, increase, decrease functions

---
+++ cart/src/reducer.js
@@ -0,0 +1,37 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItems) => cartItems.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let temp = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return {
+      ...state,
+      cart: temp,
+    };
+  }
+  if (action.type === "DECREASE") {
+    let temp = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return { ...state, cart: temp };
+  }
+  return state;
+};
+
+export default reducer;
